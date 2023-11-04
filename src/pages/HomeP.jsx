c7d23e4fc31d28d939a803db8d511dbdb44e3af4
“melibayev” <melibayefff1@gmail.com>
2023-11-05 03:20:02 +0500

    smth changed

--- src/pages/HomeP.jsx
+++ src/pages/HomeP.jsx
@@ -1,6 +1,5 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { useMemo } from "react";
-import { useEffect } from "react";
 import {
   MaterialReactTable,
   useMaterialReactTable,
@@ -12,37 +11,20 @@ import VIDEO from "../assets/video/video.MOV";
 
 
 import IMG1 from '../assets/images/img1.jpg'
-import IMG2 from '../assets/images/img2.jpg'
 import IMG3 from '../assets/images/img3.jpg'
 import IMG4 from '../assets/images/img4.jpg'
 import IMG5 from '../assets/images/img5.jpg'
 import IMG6 from '../assets/images/img6.jpg'
 import IMG7 from '../assets/images/img7.jpg'
-import IMG8 from '../assets/images/img8.jpg'
-import IMG9 from '../assets/images/img9.jpg'
 import IMG10 from '../assets/images/img10.jpg'
 import IMG11 from '../assets/images/img11.jpg'
-import IMG12 from '../assets/images/img12.jpg'
 import IMG13 from '../assets/images/img13.jpg'
 import IMG14 from '../assets/images/img14.jpg'
-import IMG15 from '../assets/images/img15.jpg'
 import IMG16 from '../assets/images/img16.jpg'
-import IMG17 from '../assets/images/img17.jpg'
-import IMG18 from '../assets/images/img18.jpg'
 import IMG19 from '../assets/images/img19.jpg'
-import IMG20 from '../assets/images/img20.jpg'
-import Slider from "react-slick";
 
 const HomeP = () => {
-  useEffect(() => {
-    const getApi = async () => {
-      const data = "https://www.gstatic.com/feedback/api.js";
-      return fetch(data)
-        .then((res) => res.json())
-        .then((d) => console.log(d));
-    };
-    // getApi()
-  }, []);
+
 
   // Room info start
   const columns = useMemo(() => [
@@ -98,15 +80,6 @@ const HomeP = () => {
     },
   });
   // Room info end
-
-  function srcset(image, size, rows = 1, cols = 1) {
-    return {
-      src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-      srcSet: `${image}?w=${size * cols}&h=${
-        size * rows
-      }&fit=crop&auto=format&dpr=2 2x`,
-    };
-  }
 
   return (
     <Fragment>
